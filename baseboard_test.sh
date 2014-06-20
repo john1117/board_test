8fe836516b434c5a1078945bc951a040ef6d0962
John lee <john.lee@hardkernel.com>
2014-06-20 10:30:19 +0000

    fix baseboard test script

--- baseboard_test.sh
+++ baseboard_test.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
-# USB test
-echo -n "usb port count : " >> /dev/ttyAMA0
-lsusb | grep "Dongle" | wc -l >> /dev/ttyAMA0
+baseboard_test() {
 
-# Ethernet test
-ping -q -c5 google.com
-if [ $? -eq 0 ]
-then
-	echo "Ethernet ok" >> /dev/ttyAMA0
-else
-	echo "Ethernet faile" >> /dev/ttyAMA0
-fi
+	# Audio Test
+	amixer cset numid=3 1
+	aplay /home/pi/police_s.wav &
 
-# Audio Test
-aplay police_s.wav
+	# USB test
+	count=0
+	count=$(lsusb | grep "Dongle" | wc -l)
+	if [ $count -eq 4 ]
+	then
+		echo "USB OK" >> /dev/ttyAMA0
+	else
+		echo "USB FAIL" >> /dev/ttyAMA0
+		echo "USB FAIL" >> /dev/ttyAMA0
+		echo "USB FAIL" >> /dev/ttyAMA0
+		echo "USB FAIL" >> /dev/ttyAMA0
+		echo "USB FAIL" >> /dev/ttyAMA0
+	fi
+
+	# Ethernet test
+	ping -q -c5 google.com
+	if [ $? -eq 0 ]
+	then
+		echo "Ethernet OK" >> /dev/ttyAMA0
+	else
+		echo "Ethernet FAIL" >> /dev/ttyAMA0
+		echo "Ethernet FAIL" >> /dev/ttyAMA0
+		echo "Ethernet FAIL" >> /dev/ttyAMA0
+		echo "Ethernet FAIL" >> /dev/ttyAMA0
+		echo "Ethernet FAIL" >> /dev/ttyAMA0
+	fi
+}
+
+case "$1" in
+start)
+	echo "############ baseboard_test start !! ############" >> /dev/ttyAMA0
+	baseboard_test
+	echo "############ baseboard_test end !! ############" >> /dev/ttyAMA0
+	;;
+stop)
+	;;
+restart)
+	;;
+esac
+exit 0
